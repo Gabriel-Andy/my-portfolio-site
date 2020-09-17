93bf5bc4f36527dbd8072144b6ace78c5434fc12
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2020-09-17 17:34:37 -0400

    I added a new images for the projects section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,44 @@ import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import Services from "../components/Services"
 import Jobs from "../components/Jobs"
-
-export default function Home() {
+import Projects from "../components/Projects"
+const Home = ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
   return (
     <Layout>
       <Hero />
       <Services />
       <Jobs title="experience" />
+      <Projects projects={projects} title="featured projects" showLinks />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          name
+          id
+        }
+      }
+    }
+  }
+`
+
+export default Home
